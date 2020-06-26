acc6dd5ee3b022d437f90a006148a5dcbaac2432
sondeokhyeon <sondeokhyeon@gmail.com>
2020-06-26 16:08:32 +0900

    Modify : Request URL 변경

--- src/util/API.js
+++ src/util/API.js
@@ -3,14 +3,14 @@ import axios from "axios";
 export async function getMinorList(arg) {
   const { param } = arg;
   const response = await axios.get(
-    `https://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${param}`
+    `http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${param}`
   );
   return response.data;
 }
 
 export async function getMajorList(arg) {
   const { major, param } = arg;
-  const url = `https://api.visitkorea.or.kr/openapi/service/rest/KorService/${major}?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${param}`;
+  const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/${major}?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${param}`;
   const response = await axios.get(url);
   return response.data;
 }
@@ -18,8 +18,8 @@ export async function getMajorList(arg) {
 export async function getDetailInfo(arg) {
   const { contentId, itemNo } = arg;
   const url = `contentTypeId=${itemNo}&contentId=${contentId}&MobileOS=ETC&MobileApp=AppTest&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&transGuideYN=Y`;
-  const id_info = `https://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${url}`;
-  const item_info = `https://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${url}`;
+  const id_info = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${url}`;
+  const item_info = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?ServiceKey=%2B6kreK3SlK%2FHeXSglkdHXVcOjgM%2BHoHwQK%2BbDXAlMNTwbkNSgJXPlywyo7CO1ntAZ5CDfYU4xFI1p%2F9TJ3fbFw%3D%3D&${url}`;
   const info = await axios.get(id_info);
   const intro = await axios.get(item_info);
   return {
